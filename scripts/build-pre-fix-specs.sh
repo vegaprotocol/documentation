0b4248f73c8c471e8b05c7e8aa85186fd3f57526
Edd <edd@vega.xyz>
2022-10-26 17:28:25 +0100

    Use template to avoid the need to remove newlines

--- scripts/build-pre-fix-specs.sh
+++ scripts/build-pre-fix-specs.sh
@@ -8,11 +8,6 @@ echo "Pre-fixup: $doc_version"
 echo "Swagger: Add version in"
 sed -i -E "s/version not set/$version/g" "specs/$doc_version/*.swagger.json"
 
-echo "Swagger: Destroy multi line summaries"
-# These are some ugly sed lines, but it matches summaries with linebreaks and only includes what's before the first linebreak
-sed -i -E 's/"summary": "\(.*[^\]\)\\n\(.*\)"/"summary": "\1"/g' "specs/$doc_version/trading_data_v1.swagger.json"
-sed -i -E 's/"summary": "\(.*[^\]\)\\n\(.*\)"/"summary": "\1"/g' "specs/$doc_version/trading_data_v2.swagger.json"
-
 echo "Swagger: Remove trailing punctuation from summaries"
 # These are some ugly sed lines too, but removes trailing , or .
 sed -i -E 's/"summary": "\(.*[^,.]\).*"/"summary": "\1"/g' "specs/$doc_version/trading_data_v1.swagger.json"
