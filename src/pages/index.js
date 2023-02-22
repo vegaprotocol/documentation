d370c63c6e8a002f48914bf34e8682de40b8c90e
Edd <edd@vega.xyz>
2023-02-22 12:23:42 +0000

    fix: homepage uses selected version

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,61 @@ import clsx from "clsx";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
+import Link from '@docusaurus/Link';
+import useIsBrowser from '@docusaurus/useIsBrowser';
+import {usePluginData} from '@docusaurus/useGlobalData';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <a class="cta" href="/mainnet/concepts/new-to-vega">New to Vega?</a>
+        <Link className="cta" href="concepts/new-to-vega">New to Vega?</Link>
       </div>
     </header>
   );
 }
 
+// This feels hacky - but Link doesn't appear to prefix version on the hompage
+// presumably because it's not served by Docs
+const DEFAULT = 'mainnet'
+
+// Given an array of versions, and a selected version (see below), return the
+// appropriate base path for the version
+function getSelectedBase(versions, selectedVersion) {
+   if (!selectedVersion) {
+    return DEFAULT
+   }
+
+   const chosenVersion = versions.find(ver => ver.name === selectedVersion)
+   if (!chosenVersion) {
+    return DEFAULT
+   }
+
+   return chosenVersion.path
+}
+
+// Detect if the user has selected a version, gather plugin information together
+// and select a base url for homepage links
+function getBaseUrl(versions) {
+  if (!window || !window.localStorage || !versions || !versions.versions) {
+    return DEFAULT
+  }
+
+  const v = window.localStorage.getItem('docs-preferred-version-default') || DEFAULT
+   
+  return getSelectedBase(versions.versions, v)
+}
+
 export default function Home() {
-  const { siteConfig, siteMetadata } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+  const versions = usePluginData('docusaurus-plugin-content-docs');
+  const isBrowser = useIsBrowser();
+  const base = isBrowser ? getBaseUrl(versions) : 'mainnet'
+  
   return (
     <Layout description={`${siteConfig.tagline}`}>
       <HomepageHeader />
@@ -28,44 +67,44 @@ export default function Home() {
             <h2>Getting started</h2>
             <p>Learn about the Vega framework and get set up to take part.</p>
             <ul>
-              <li><a href="/mainnet/concepts/new-to-vega">Introduction to Vega</a></li>
-              <li>Get a <a href="/mainnet/tools/vega-wallet">Vega wallet</a>, on <a href="/mainnet/tools/vega-wallet/desktop-app">desktop</a> or for <a href="/mainnet/tools/vega-wallet/cli-wallet">command line</a></li>
-              <li><a class="external" href="https://github.com/orgs/vegaprotocol/projects/114/views/4" target="_blank">Roadmap</a></li>
+              <li><Link to={`${base}/concepts/new-to-vega`}>Introduction to Vega</Link></li>
+              <li>Get a <Link to={`${base}/tools/vega-wallet`}>Vega wallet</Link>, on <Link to={`${base}/tools/vega-wallet/desktop-app`}>desktop</Link> or for <Link to={`${base}/tools/vega-wallet/cli-wallet`}>command line</Link></li>
+              <li><Link className="external" base="https://github.com/orgs/vegaprotocol/projects/114/views/4" target="_blank">Roadmap</Link></li>
             </ul>
           </article>
           <article>
             <h2>Governance</h2>
             <p>The network is governed by the community. Take part by voting on proposals and proposing changes.</p>
             <ul>
-              <li><a href="/mainnet/concepts/vega-protocol#governance">Introduction to governance</a></li>
-              <li><a href="mainnet/tutorials/proposals">Tutorials: Governance proposals</a></li>
-              <li><a class="external" href="https://token.vega.xyz/governance" target="_blank">Governance dApp</a></li>
+              <li><Link to={`${base}/concepts/vega-protocol#governance`}>Introduction to governance</Link></li>
+              <li><Link to={`${base}/mainnet/tutorials/proposals`}>Tutorials: Governance proposals</Link></li>
+              <li><Link className="external" href="https://token.vega.xyz/governance" target="_blank">Governance dApp</Link></li>
             </ul>
           </article>
           <article>
             <h2>Trading</h2>
             <p>Explore the risk-free Vega testnet, Fairground. Mainnet trading launching soon, starting with cash settled futures.</p>
             <ul>
-              <li><a class="external" href="https://console.fairground.wtf/" target="_blank">Fairground trading console</a></li>
-              <li><a href="https://docs.vega.xyz/testnet/concepts/new-to-vega" target="_blank">Fairground docs</a></li>
+              <li><Link className="external" href="https://console.fairground.wtf/" target="_blank">Fairground trading console</Link></li>
+              <li><Link href="https://docs.vega.xyz/testnet/concepts/new-to-vega" target="_blank">Fairground docs</Link></li>
             </ul>
           </article>
         </section>
         <hr/>
         <section id="spotlight">
           <h2>Spotlight</h2>
-          <a href="/mainnet/node-operators" class="card cardContainer">
-            <h3 class="cardTitle">Node operators: Overview</h3>
-            <p class="cardDescription">Get guidance on how to maintain and upgrade a node.</p>
-          </a>
-          <a href="/mainnet/releases/overview" class="card cardContainer">
-            <h3 class="cardTitle">Release summaries</h3>
-            <p class="cardDescription">See the latest features and get ahead of breaking changes.</p>
-          </a>
-          <a href="/mainnet/tools/vega-wallet/desktop-app" class="card cardContainer">
-            <h3 class="cardTitle">Vega Wallet</h3>
-            <p class="cardDescription">Step-by-step instructions to set up your Vega Wallet using the desktop app.</p>
-          </a>
+          <Link to={`${base}/node-operators`} className="card cardContainer">
+            <h3 className="cardTitle">Node operators: Overview</h3>
+            <p className="cardDescription">Get guidance on how to maintain and upgrade a node.</p>
+          </Link>
+          <Link to={`${base}/releases/overview`} className="card cardContainer">
+            <h3 className="cardTitle">Release summaries</h3>
+            <p className="cardDescription">See the latest features and get ahead of breaking changes.</p>
+          </Link>
+          <Link to={`${base}/tools/vega-wallet/desktop-app`} className="card cardContainer">
+            <h3 className="cardTitle">Vega Wallet</h3>
+            <p className="cardDescription">Step-by-step instructions to set up your Vega Wallet using the desktop app.</p>
+          </Link>
         </section>
       </main>
     </Layout>
