775ccf20bd1507b1ce5e9187e59e069ab4a59b4c
Edd <edd@vega.xyz>
2024-02-22 14:56:12 +0000

    back to no cache

--- src/plugins/webpack-docusaurus-plugin.config.js
+++ src/plugins/webpack-docusaurus-plugin.config.js
@@ -9,41 +9,46 @@ async function webpackDocusaurusPlugin(context, options) {
     name: 'webpack-docusaurus-plugin',
     configureWebpack(config, isServer, utils) {
       const isCI = process.env.CI;
+      const cacheOptions = isCI ? { cache: false } : { profile: true, type: 'filesystem' };
 
-      if (isCI === false) {
-        console.log(`️️⛏️  webpack-docusaurus-plugin: Using memory cache (${isServer ? 'server' : 'client'  })`);
+      if (cacheOptions.cache === false) {
+        console.log('ℹ️  Disabling webpack cache because Vercel sigkills');
       } else {
-        console.log(`⛏️  webpack-docusaurus-plugin: Using file system cache (brotli) (${isServer ? 'server' : 'client'  })`);
+        console.log('ℹ️  Enabling filesystem cache 🚤🚤');
       }
+
       const plugins = config.plugins.filter(p => {
-        if (p instanceof webpackbar) {
-          return false
-        }
-        return true
-      }
-      );
+        return !(p instanceof webpackbar);
+      });
+
+      // A more informative progress reporter than webpackbar
       plugins.push(new webpack.ProgressPlugin(),)
+
       return {
         // Ensure these new options get used
         mergeStrategy: {
-          'cache.compression': 'replace',
+          'cache': 'replace',
           'cache.type': 'replace',
+          'cache.profile': 'replace',
           'infrastructureLogging.level': 'replace',
           'stats.all': 'replace',
           'optimization.minimizer': 'replace',
           'plugins': 'replace'
         },
-        // Disables cache in CI
-        cache: {
-          type: isCI ? 'filesystem' : 'memory',
-          compression: 'brotli',
-        },
+        // Cache is too big for vercel
+        ...cacheOptions,
         // Turns off webpackbar
         plugins
       }
     },
     postBuild({ routesPaths, outDir }) {
       console.log(`✅  webpack-docusaurus-plugin: Built ${routesPaths.length} routes to ${outDir}`);
+
+      const isCI = process.env.CI;
+      if (isCI) {
+        rimraf.sync(path.join('node_modules/.cache'));
+        console.log(`❤️‍🔥  Ensuring no cache is left behind`);
+      }
     }
   }
 }
